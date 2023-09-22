150cddc6bc41b7fa7bf6677def76edbb3db4e54d
Jianing Zhang <jz5212@nyu.edu>
2023-09-22 10:21:05 -0400

    fixed code

--- analyze.py
+++ analyze.py
@@ -3,21 +3,30 @@
 import csv
 
 with open('/Users/zhangjianing/Desktop/Databases/2-data-munging-Catherineya/data/clean_data.csv', 'r') as f, open('/Users/zhangjianing/Desktop/Databases/2-data-munging-Catherineya/data/analyze.txt', 'w') as f2:
+
     reader = csv.reader(f, delimiter=',')
     rows = list(reader)
     for i in range(1, len(rows), 10):
         date1 = rows[i][0]
         avg = 0
         if rows[i][0] == '2020':
-            date2 = rows[i+2][0]
+            date2 = rows[i+3][0]
             numYears = 3
             for k in range(i, i+3):
-                avg += float(rows[k][13])
+                if '*' not in rows[k][13]:
+                    avg += float(rows[k][13])
+                else:
+                    numYears -= 1
+                #print(rows[k])
             avg = format(avg / numYears, '.1f')
         else:
             date2 = rows[i+9][0]
             numYears = 10
             for k in range(i, i+10):
-                avg += float(rows[k][13])
+                if '*' not in rows[k][13]:
+                    avg += float(rows[k][13])
+                else:
+                    numYears -= 1
+                #print(rows[k])
             avg = format(avg / numYears, '.1f')
         f2.write(date1 + '-' + date2 + ': ' + avg + '\n')
